faf243db6a85ab5ee3536772e9a5832fdbaf2ad3
cooi123 <calebooi@hotmail.com>
2025-05-27 11:50:55 +1000

    add deployment action

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from src.tasks.celery_tasks import create_consultant_primer
+from src.tasks.celery_tasks import create_consultant_primer, create_research_paper_summary
 from src.models.task_models import CeleryTaskRequest, TaskStatus, TaskResult
 from src.configs.celery_config import celery_app
 from src.utils.shared import send_update_to_broker
@@ -19,12 +19,28 @@ def run_priemer_task(request: CeleryTaskRequest):
     # Return task ID and status
     return {
         "task_id": task.id,
-        "status": "pending",
+        "status": TaskStatus.PENDING,
         "parent_transaction_id": request.parent_transaction_id
     }
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port=9000, reload=True)
+@app.post("/research-summarizer")
+def run_research_paper_task(request: CeleryTaskRequest):
+    print("received request", request)
+    # Send initial pending status
+    send_update_to_broker(request, TaskResult(
+        task_status=TaskStatus.PENDING,
+        parent_transaction_id=request.parent_transaction_id
+    ))
+    # Start the task
+    task = create_research_paper_summary.delay(request.model_dump())
+    # Return task ID and status
+    return {
+        "task_id": task.id,
+        "status": TaskStatus.PENDING,
+        "parent_transaction_id": request.parent_transaction_id
+    }
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run("app:app", host="0.0.0.0", port=9000, reload=True)
 
 
